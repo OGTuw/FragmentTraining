0b4618dc2b656d1871523aeb3d2c72652a5fd7d5
takechee <takechee14@gmail.com>
2020-04-25 09:22:36 +0900

    より良い書き方

--- app/src/main/java/com/example/fragmenttraining/FragmentButton.java
+++ app/src/main/java/com/example/fragmenttraining/FragmentButton.java
@@ -10,29 +10,57 @@ import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
-public class FragmentButton extends Fragment{
+import org.jetbrains.annotations.NotNull;
+
+public class FragmentButton extends Fragment {
+
+    // bundleとかで渡すパラメータ名は定数化しておく
+    public static final String PARAM_BUTTON_ID = "buttonId";
+
+
+    // =============================================================================================
+    //
+    // Lifecycle
+    //
+    // =============================================================================================
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+    ) {
         // R.layout.Fragment(xml)のファイル名
-        return inflater.inflate(R.layout.button, null);
-
+        return inflater.inflate(R.layout.button, container, false);
     }
 
     @Override
-    public void onActivityCreated (Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public void onViewCreated(@NotNull View view, @Nullable Bundle savedInstanceState) {
+        setupButtonPattern1(view);
+    }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+
+    // =============================================================================================
+    //
+    // Setup
+    //
+    // =============================================================================================
+    private void setupButtonOriginal(View view) {
         // ボタンを取得。
-        ImageButton btnMimikyu = getView().findViewById(R.id.button_mimikyu);
-        ImageButton btnDragovish = getView().findViewById(R.id.button_doragovish);
-        ImageButton btnHatterene = getView().findViewById(R.id.button_hatterene);
+        ImageButton btnMimikyu = view.findViewById(R.id.button_mimikyu);
+        ImageButton btnDragovish = view.findViewById(R.id.button_doragovish);
+        ImageButton btnHatterene = view.findViewById(R.id.button_hatterene);
 
         // ボタンイベントを作る。
         // Intentクラスの変数にbuttonIdとしてそれぞれのボタンをセット。
@@ -41,7 +69,7 @@ public class FragmentButton extends Fragment{
             @Override
             public void onClick(View v) {
                 Intent data = new Intent();
-                data.putExtra("buttonId",ButtonId.Mimikyu );
+                data.putExtra("buttonId", ButtonId.Mimikyu);
                 getActivity().onActivityReenter(Activity.RESULT_OK, data);
             }
         });
@@ -50,7 +78,7 @@ public class FragmentButton extends Fragment{
             @Override
             public void onClick(View v) {
                 Intent data = new Intent();
-                data.putExtra("buttonId",ButtonId.Dragovish );
+                data.putExtra("buttonId", ButtonId.Dragovish);
                 getActivity().onActivityReenter(Activity.RESULT_OK, data);
             }
         });
@@ -59,16 +87,90 @@ public class FragmentButton extends Fragment{
             @Override
             public void onClick(View v) {
                 Intent data = new Intent();
-                data.putExtra("buttonId",ButtonId.Hatterene );
+                data.putExtra("buttonId", ButtonId.Hatterene);
                 getActivity().onActivityReenter(Activity.RESULT_OK, data);
             }
         });
-
     }
 
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
+    // =============================================================================================
+    //
+    // パターン１
+    //
+    // =============================================================================================
+    private void setupButtonPattern1(View view) {
+        // 共通のクリックリスナーを作る
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ButtonId buttonId;
+                switch (v.getId()) {
+                    case R.id.button_mimikyu:
+                        buttonId = ButtonId.Mimikyu;
+                        break;
+                    case R.id.button_doragovish:
+                        buttonId = ButtonId.Dragovish;
+                        break;
+                    case R.id.button_hatterene:
+                        buttonId = ButtonId.Hatterene;
+                        break;
+                    default:
+                        return;
+                }
+
+                Intent data = new Intent();
+                data.putExtra(PARAM_BUTTON_ID, buttonId);
+                FragmentActivity activity = getActivity();
+                if (activity != null) {
+                    activity.onActivityReenter(Activity.RESULT_OK, data);
+                }
+            }
+        };
+
+        // リスナーをそれぞれ登録
+        view.findViewById(R.id.button_mimikyu).setOnClickListener(clickListener);
+        view.findViewById(R.id.button_doragovish).setOnClickListener(clickListener);
+        view.findViewById(R.id.button_hatterene).setOnClickListener(clickListener);
+    }
+
+
+    // =============================================================================================
+    //
+    // パターン２
+    //
+    // =============================================================================================
+    private void setupButtonPattern2(View view) {
+        view.findViewById(R.id.button_mimikyu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activityReenter(ButtonId.Mimikyu);
+            }
+        });
+
+        view.findViewById(R.id.button_doragovish).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activityReenter(ButtonId.Dragovish);
+            }
+        });
+
+        view.findViewById(R.id.button_hatterene).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activityReenter(ButtonId.Hatterene);
+            }
+        });
+    }
+
+    // 共通の処理はメソッド化しておいて、異なる部分だけ引数で受け取る
+    private void activityReenter(ButtonId buttonId) {
+        Intent data = new Intent();
+        data.putExtra(PARAM_BUTTON_ID, buttonId);
+        FragmentActivity activity = getActivity();
+        if (activity != null) {
+            activity.onActivityReenter(Activity.RESULT_OK, data);
+        }
     }
 
 }
